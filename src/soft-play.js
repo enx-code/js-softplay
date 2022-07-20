202ebf1db4e3bfb39565f506ea9b3fc8bd14bdd1
Enky <enkh6at@yahoo.com>
2022-07-20 15:27:45 +0100

    still have 2 fails in my jasmine

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,6 @@
 // do not change these lines
 
-function reset () {
+function reset() {
   adults = 0
   children = 0
 }
@@ -10,11 +10,89 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
+let totalAdults = 0
+let totalChildren = 0
+
+function enter(numAdults, numChildren) {
+  let result = true
+  
+  if (numAdults >= numChildren) {
+    adults += numAdults
+    children += numChildren
+  } else {
+    result = false
+  }
+  
+  return result
+}
+
+function leave(numAdults, numChildren) {
+  let result = true
+ 
+  if (numAdults !== numChildren) {
+    return false
+  }
+  
+  return result
+}
+
+function occupancy() {
+  const occupancyInformation = {
+    adults: adults,
+    children: children
+  }
+  return occupancyInformation
+}
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@CLASS CODE
+
+
+// function occupancy() {
+//   return {
+//     adults, 
+//     children
+//   }
+
+// }
+
+// function enter(numAdults, numChildren) {
+//   if (numAdults < numChildren) {
+//     return false
+//   }
+//   adults += numAdults
+//   children += numChildren
+//   totalAdults += numAdults
+//   totalChildren += numChildren
+//   return true
+// }
+
+// function leave(numAdults, numChildren) {
+//   if (numAdults < numChildren) {
+//     return false 
+//   } else if (numAdults > adults || numChildren > children) {
+//     return false
+//   } else if (children - numChildren > adults - numAdults) {
+//     return false
+//   }
+//   adults -= numAdults
+//   children -= numChildren
+//   return true
+
+// }
+
+// function total() {
+//   return {
+//     adults: totalAdults,
+//     children: totalChildren
+//   }
+// }
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
   reset: reset
 }
